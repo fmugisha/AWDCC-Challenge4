db5afc5ef5e712064175028dbef1b4410d7c9c6b
fmugisha <54206544+fmugisha@users.noreply.github.com>
2021-08-28 17:36:06 +0200

    Add files via upload

---
+++ firestore.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDbPSMpToR9wrQxt-7jtjLKJAHTep2CLGI",
+    authDomain: "todo-list-157fa.firebaseapp.com",
+    projectId: "todo-list-157fa",
+    storageBucket: "todo-list-157fa.appspot.com",
+    messagingSenderId: "680624861404",
+    appId: "1:680624861404:web:431488a2de04bd45750419",
+    measurementId: "G-6VFS5D20XR"
+  };
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
